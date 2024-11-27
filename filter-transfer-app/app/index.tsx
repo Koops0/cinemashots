b0711363c26f5c7a3fea8fbf2d8e96562cdeee7f
Kershan Arulneswaran <74938655+Koops0@users.noreply.github.com>
2024-11-27 14:19:34 -0500

    finished permissions (works on android)

--- filter-transfer-app/app/index.tsx
+++ filter-transfer-app/app/index.tsx
@@ -1,4 +1,19 @@
 import {ThemedText} from "@/components/ThemedText";
+import { Redirect, useRouter } from "expo-router";
+import {Camera, useCameraDevice, useCameraPermission} from 'react-native-vision-camera';
 export default function HomeScreen(){
-    return <ThemedText>Hello World!</ThemedText>;
+    const {hasPermission} = useCameraPermission();
+    const micPermission = Camera.getMicrophonePermissionStatus();
+    const redirectPerms = !hasPermission || micPermission === "not-determined";
+    const dev = useCameraDevice('back');
+    const router = useRouter();
+
+    if(redirectPerms) return <Redirect href={"./permissions"}/>;
+    if(!dev) return <></>;
+
+    return (
+        <ThemedText>
+        Hello World!
+        </ThemedText>
+    )
 }
--- filter-transfer-app/app/permissions.tsx
+++ filter-transfer-app/app/permissions.tsx
@@ -3,23 +3,106 @@ import {Camera, CameraPermissionStatus} from 'react-native-vision-camera'
 import * as ExpoMediaLibrary from 'expo-media-library';
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Switch, TouchableOpacity, View, Alert } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
+const ICON_SIZE = 32;
 
-export default function PermissionsS(){
+export default function Permissions(){
+    const router = useRouter();
     const [permissionStatus, setPermissionStatus] = 
         React.useState<CameraPermissionStatus>("not-determined");
     const [micPermissionStatus, setMicPermissionStatus] = 
         React.useState<CameraPermissionStatus>("not-determined");
     const [mediaLibraryPermissions, requestMediaLibraryPermissions] =
         ExpoMediaLibrary.usePermissions();
-    
-    return(
-        <>
-           <ThemedView style={styles.container}>
+
+    const requestCameraPermission = async () => {
+        const permissions = await Camera.requestCameraPermission();
+        setPermissionStatus(status);
+    }
+
+    const requestMicrophonePermission = async () => {
+        const permissions = await Camera.requestMicrophonePermission();
+        setMicPermissionStatus(status);
+    }
+
+    const handleContinue = () => {
+        if(permissionStatus === "granted" && micPermissionStatus === "granted" && mediaLibraryPermissions?.granted){
+            router.replace("/");
+        }else{
+            Alert.alert("Please enable all permissions to continue.");
+        }
+    }
+
+    const PermissionsContent = () => (
+        <ThemedView style={styles.container}>
             <ThemedText type="subtitle" style={styles.subtitle}>
                 CinemaShots requires access to your camera and media library to work properly.
             </ThemedText>    
+        </ThemedView>
+    );
+    
+    return(
+        <>
+        <Stack.Screen options={{headerTitle: "Permissions"}}/>
+           <ThemedView style={styles.container}>
+            <View style={styles.spacer}/>
+            <ThemedText type="subtitle" style={styles.subtitle}>
+                CinemaShots requires access to your camera and media library to work properly.
+            </ThemedText>
+            <View style={styles.spacer}/>
+            <View style={styles.row}>
+                <Ionicons name="lock-closed-outline" size={ICON_SIZE} color="blue"/>
+                <ThemedText style={styles.footnote}>
+                    Required Permissions
+                </ThemedText>
+            </View>
+            <View style={styles.spacer}/>
+            <View style={StyleSheet.compose(styles.row, styles.permissionContainer)}>
+                <Ionicons name="camera-outline" size={ICON_SIZE} color="blue"/>
+                <View style={styles.permissionText}>
+                    <ThemedText type="subtitle">Camera</ThemedText>
+                    <ThemedText>This permission must be enabled to take videos and photos.</ThemedText>
+                </View>
+                <Switch trackColor={{true: "blue"}}
+                    value={micPermissionStatus === "granted"}
+                    onChange={requestCameraPermission}
+                />
+            </View>
+            <View style={styles.spacer}/>
+            <View style={StyleSheet.compose(styles.row, styles.permissionContainer)}>
+                <Ionicons name="mic-outline" size={ICON_SIZE} color="blue"/>
+                <View style={styles.permissionText}>
+                    <ThemedText type="subtitle">Microphone</ThemedText>
+                    <ThemedText>This permission must be enabled to capture audio for videos.</ThemedText>
+                </View>
+                <Switch trackColor={{true: "blue"}}
+                    value={mediaLibraryPermissions?.granted}
+                    onChange={requestMicrophonePermission}
+                />
+            </View>
+            <View style={styles.spacer}/>
+            <View style={StyleSheet.compose(styles.row, styles.permissionContainer)}>
+                <Ionicons name="camera-outline" size={ICON_SIZE} color="blue"/>
+                <View style={styles.permissionText}>
+                    <ThemedText type="subtitle">Photo Library</ThemedText>
+                    <ThemedText>This permission must be enabled for filters and accessing photos and videos.</ThemedText>
+                </View>
+                <Switch trackColor={{true: "blue"}}
+                    value={permissionStatus === "granted"}
+                    onChange={async() => await requestMediaLibraryPermissions()}
+                />
+            </View>
+            <View style={styles.spacer}/>
+            <View style={styles.spacer}/>
+            <View style={styles.spacer}/>
+            <TouchableOpacity style={StyleSheet.compose(styles.row, styles.continueButton)}
+                onPress={handleContinue}>
+                <Ionicons name="arrow-forward-outline" size={ICON_SIZE} color="black"/>
+                <ThemedText>Continue</ThemedText>
+            </TouchableOpacity>
            </ThemedView>
         </>
     );
@@ -27,10 +110,40 @@ export default function PermissionsS(){
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        padding: 20,
+      flex: 1,
+      padding: 20,
     },
     subtitle: {
-        textAlign: 'center',
-    }
-});
+      textAlign: "center",
+    },
+    footnote: {
+      fontSize: 12,
+      fontWeight: "bold",
+      letterSpacing: 2,
+    },
+    row: {
+      flexDirection: "row",
+      alignItems: "center",
+      gap: 6,
+    },
+    spacer: {
+      marginVertical: 8,
+    },
+    permissionContainer: {
+      backgroundColor: "#fffff0",
+      borderRadius: 10,
+      padding: 10,
+      justifyContent: "space-between",
+    },
+    permissionText: {
+      marginLeft: 10,
+      flexShrink: 1,
+    },
+    continueButton: {
+      padding: 10,
+      borderWidth: 2,
+      borderColor: "black",
+      borderRadius: 50,
+      alignSelf: "center",
+    },
+  });
